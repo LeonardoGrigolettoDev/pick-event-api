2d6a0d386e2ea3865f013882a536ea2d8357c681
DiiasDev <devpdias@gmail.com>
2025-05-12 16:56:11 -0300

    Adição de lógica para metodo post

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,22 @@
 package routes
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine) {
 	// auth := router.Group("/")
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	api := router.Group("/api")
 	// api.Use(middlewares.AuthMiddleware())
 	{
